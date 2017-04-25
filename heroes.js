b2a4680bce38d59a3e3932505f39150fa2a05c7e
Spencer West <spencer.west@kbb.com>
2017-04-25 16:39:29 -0700

    updates dynamic logic

--- heroes.js
+++ heroes.js
@@ -4,71 +4,72 @@ var array = [["Alarak", "Chromie", "Falstad", "Greymane", "Gul'dan", "Kael'thas"
 
 ["Abathur", "Auriel", "Brightwing", "Chen", "Dehaka", "E.T.C.", "Johanna", "Kharazim", "Li Li", "Lt. Morales", "Lúcio", "Malfurion", "Medivh", "Muradin", "Rehgar", "Stitches", "Tassadar", "Tyrael", "Uther", "Zarya"],
 
-["Alarak", "Anub'arak", "Artanis", "Arthas", "Chen", "Diablo", "Dehaka", "Illidan", "Kharazim", "Leoric", "Muradin", "Ragnaros", "Rexxar", "Samuro", "Sonya", "Stitches", "Thrall", "Varian", "Xul", "Zarya"]]
-
-var myList = ["Zeratul", "Valla", "Nazeebo", "E.T.C.", "Nova", "Stiches", "Li Li", "Xul", "Illidan", "Sgt. Hammer", "Zagara", "Sonya", "Sylvanas", "Muradin", "Murky", "Cho", "Gall", "Tassadar", "Raynor", "Jaina", "Falstad", "Malfurion", "Greymane", "Kerrigan", "Tracer", "Azmodan", "Li-Ming", "Diablo", "Gaz", "Zarya", "Auriel"];
+["Alarak", "Anub'arak", "Artanis", "Arthas", "Chen", "Diablo", "Dehaka", "Illidan", "Kharazim", "Leoric", "Muradin", "Ragnaros", "Rexxar", "Samuro", "Sonya", "Stitches", "Thrall", "Varian", "Xul", "Zarya"]];
 
 var allHeroes = ["Abathur", "Alarak", "Anub'arak", "Artanis", "Arthas", "Auriel", "Azmodan", "Brightwing", "The Butcher", "Cassia", "Chen", "Cho'gall", "Chromie", "Dehaka", "Diablo", "E.T.C.", "Falstad", "Gazlowe", "Genji", "Greymane", "Gul'dan", "Illidan", "Jaina", "Johanna", "Kael'thas", "Kerrigan", "Kharazim", "Leoric", "Li Li", "Li-Ming", "The Lost Vikings", "Lt. Morales", "Lunara", "Lúcio", "Malfurion", "Medivh", "Muradin", "Murky", "Nazeebo", "Nova", "Probius", "Ragnaros", "Raynor", "Rehgar", "Rexxar", "Samuro", "Sgt. Hammer", "Sonya", "Stitches", "Sylvanas", "Tassadar",  "Thrall", "Tracer", "Tychus", "Tyrael", "Tyrande", "Uther", "Valeera", "Valla", "Varian", "Xul", "Zagara", "Zarya", "Zeratul", "Zul'jin"];
 
 var userList = [];
-
 var results = [];
-var order = function() {
-  for(var i=0; i<4; i++) {
-    var myObj = {};
-    myObj.count = difference(array[i], userList).length;
-    myObj.heroList = difference(array[i], userList);
-    switch (i) {
-      case 0:
-        myObj.arrayName = "Assassin";
-
-        break;
-      case 1:
-        myObj.arrayName = "Flex";
-
-        break;
-      case 2:
-        myObj.arrayName = "Support & Specialist";
-
-        break;
-      case 3:
-        myObj.arrayName = "Tanks & Bruisers";
-
-        break;
-      default:
-        break;
-
-    }
-    results.push(myObj)
-  }
-
-  results.sort(function(a, b) {
-      return a.count - b.count;
-  });
-}
-
-
-function difference(a1, a2) {
-  var result = [];
-  for (var i = 0; i < a1.length; i++) {
-    if (a2.indexOf(a1[i]) === -1) {
-      result.push(a1[i]);
-    }
-  }
-  return result;
-}
 
 $(document).ready(function() {
+  var order = function() {
+    results = [];
+    var myObj = {};
+    for(var i=0; i<4; i++) {
+      myObj.count = difference(array[i], userList).length;
+      myObj.heroList = difference(array[i], userList);
+      switch (i) {
+        case 0:
+          myObj.arrayName = "Assassin";
+
+          break;
+        case 1:
+          myObj.arrayName = "Flex";
+
+          break;
+        case 2:
+          myObj.arrayName = "Support & Specialist";
+
+          break;
+        case 3:
+          myObj.arrayName = "Tanks & Bruisers";
+
+          break;
+        default:
+          break;
+
+      }
+      results.push(myObj)
+    }
+
+    results.sort(function(a, b) {
+        return a.count - b.count;
+    });
+  }
+
+  function difference(a1, a2) {
+    var result = [];
+    for (var i = 0; i < a1.length; i++) {
+      if (a2.indexOf(a1[i]) === -1) {
+        result.push(a1[i]);
+      }
+    }
+    return result;
+  }
+
   function inject() {
+    $('#injectHTML').append('<div id="dynamic"></div>');
     for(var i = results.length-1; i > -1; i--) {
-      $('#injectHTML').append('<li class="list">' +  '<div><b>Bundle Name:</b> ' + results[i].arrayName + '</div>' + '<div><b>Number of heroes gained:</b> ' + results[i].count + '</div>' + '<div><b>List of heroes gained:</b> ' + results[i].heroList.join(", ") + '</div>' + '</li>');
+      $('#dynamic').append('<li class="list">' +  '<div><b>Bundle Name:</b> ' + results[i].arrayName + '</div>' + '<div><b>Number of heroes gained:</b> ' + results[i].count + '</div>' + '<div><b>List of heroes gained:</b> ' + results[i].heroList.join(", ") + '</div>' + '</li>');
     }
   }
+
   for(var i=0; i < allHeroes.length; i++) {
     $('#allHeroes').append('<input type="checkbox" class="hero" name="checkbox" value="' + allHeroes[i] + '">' + allHeroes[i] + '<br>');
   }
+
   $('#submit').on('click', function(){
-    $('.list').remove();
+    $(this).closest('#wrapper').find('#dynamic').remove();
     var checkedValue = $('.hero:checked');
     for (var i = 0; i <checkedValue.length; i++) {
       userList.push(checkedValue[i].value);

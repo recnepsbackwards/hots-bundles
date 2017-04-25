7a364a1693b83269e4c41396523f836a8e374d74
Spencer <spencer@postings.com>
2017-04-24 23:24:45 -0700

    first commit

---
+++ heroes.js
@@ -0,0 +1,7 @@
+var dmgList = ['Alarak', 'Chromie', 'Falstad', 'Greymane', 'Gul-dan', 'Kael-thas', 'Kerrigan', 'Li-Ming', 'Lunara', 'Nazeebo', 'Nova', 'Raynor', 'Sylvanas', 'The Butcher', 'Tracer', 'Tychus', 'Valeera', 'Valla', 'Zeratul', 'Zul-jin']
+
+var flexList = ['Abathur', 'Arthas', 'Azmodan', 'Brightwing', 'Diablo', 'E.T.C.', 'Greymane', 'Johanna', 'Kerrigan', 'Li Li', 'Li-Ming', 'Lt. Morales', 'Nazeebo', 'Rehgar', 'Rexxar', 'Sylvanas', 'The Lost Vikings', 'Tychus', 'Varian', 'Zul-jin']
+
+var supportList = ['Abathur', 'Auriel', 'Brightwing', 'Chen', 'Dehaka', 'E.T.C.', 'Johanna', 'Kharazim', 'Li Li', 'Lt. Morales', 'Lúcio', 'Malfurion', 'Medivh', 'Muradin', 'Rehgar', 'Stitches', 'Tassadar', 'Tyrael', 'Uther', 'Zarya']
+
+var tankList = ['Alarak', 'Anub-arak', 'Artanis', 'Arthas', 'Chen', 'Diablo', 'Dehaka', 'Illidan', 'Kharazim', 'Leoric', 'Muradin', 'Ragnaros', 'Rexxar', 'Samuro', 'Sonya', 'Stitches', 'Thrall', 'Varian', 'Xul', 'Zarya']
